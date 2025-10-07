2228861f18a726469491736922ef82b8c8d89de1
Yves Scherdin <yves.scherdin@gmx.de>
2025-10-07 20:49:26 +0200

    +cls perception data

---
+++ CSharp/Unity/AI/Source/Data/PerceptionData.cs
@@ -0,0 +1,18 @@
+﻿namespace MageGame.AI.Data
+{
+    [System.Serializable]
+    public class PerceptionData
+    {
+        public PerceptionType type;
+        public float range;
+        public float outOffset;
+    }
+
+    public enum PerceptionType
+    {
+        Unspecified = 0,
+        Proximity = 1,
+        Visual = 2,
+        Auditive = 3
+    }
+}
